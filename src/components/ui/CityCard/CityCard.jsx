0fcd85ecc39fc688b3a03228a0aec96576efef1b
terrxrmachine <so3.workspace@gmail.com>
2024-11-29 13:06:25 +0700

    build fix

--- src/components/ui/CityCard/CityCard.jsx
+++ src/components/ui/CityCard/CityCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./CityCard.module.css";
+import styles from "./Citycard.module.css";
 
 const CityCard = ({ name, image, price }) => (
   <button className={styles.card}>
